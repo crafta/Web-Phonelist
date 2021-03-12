c66e65a6893ed064bba924d13db733a8d16f3fc8
iTzMeRafa <rafael97@gmx.de>
2021-03-12 12:07:43 +0100

    initialize project

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/***
+ * Eine Web Telefonliste mit Such- und Filterfunktionen.
+ * @copyright Stadt Kamen 2021
+ */
+?>
+<!DOCTYPE html>
+<html lang="de">
+<meta charset="UTF-8">
+<title>Stadt Kamen Telefonliste</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap-utilities.min.css">
+<style>
+</style>
+<!-- JavaScript-->
+<script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<body>
+
+<div class="row">
+    <div class="col-md-4">1</div>
+    <div class="col-md-4">2</div>
+    <div class="col-md-4">3</div>
+</div>
+</body>
+</html>
